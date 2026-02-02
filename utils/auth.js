c4f731e630b8a2816a287c61d7d888611d05515e
Inés <perezvaleraines@gmail.com>
2026-02-02 13:32:34 +0100

    nuevo pdf y corrección de sintaxis

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,6 @@
-let autenticacion = (req, res, next) => {
+export const autenticacion = (req, res, next) => {
     if (req.session && req.session.usuario)
         return next();
     else
         res.redirect('/auth/login');
-};
-
-module.exports = {
-    autenticacion: autenticacion
 };
